49262c4cd0ff2909b1e41cd1f1601a99c20de9d9
yury noh <dliam123@naver.com>
2016-09-24 18:48:31 +0900

    B03.java 코딩 완료

--- B03.java
+++ B03.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
-class B01 {
-	int birthYear;
-	int age;
+class B03 {
+	int days;
 
 	public void init() {
 		input();
@@ -12,20 +11,19 @@ class B01 {
 	public void input() {
 		Scanner s = new Scanner(System.in);
 
-		System.out.println("태어난 년도를 입력하세요: ");
-		this.birthYear = s.nextInt();
-
-		this.age = (2014 - birthYear) + 1;
+		System.out.println("날수를 입력하세요: ");
+		this.days = s.nextInt();
 	}
 
 	public void print() {
-		if(age <= 20)
-			System.out.println("미성년자 입니다.");
-		else
-			System.out.println("성인 입니다.");
+		int seconds = days * 24* 60 * 60;
+		int m_count = seconds / 1000000;
+
+		System.out.println("날수에 해당되는 기간은 모두 " + seconds + " 초 입니다.");
+		System.out.println("100만 초가 모두 " + m_count + "번이나 포함됩니다.");
 	}
 
 	public static void main(String args[]) {
-		new B01().init();
+		new B03().init();
 	}
 }
