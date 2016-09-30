ff977ec860ef410b6f62916a02f5d4833402767b
yury noh <dliam123@naver.com>
2016-09-30 10:39:08 +0900

    A06 퀴즈 잘못된 제목으로 인해서 다시 푸쉬

---
+++ A06.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class A06 {
+	String name;
+	int korScore;
+	int mathScore;
+	int engScore;
+	public void run() {
+		this.input();
+		this.print();
+	}
+	public void input() {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("이름 :");
+		this.name = s.next();
+		System.out.print("국어점수 입력: ");
+		this.korScore = s.nextInt();
+		System.out.print("수학점수 입력: ");
+		this.mathScore = s.nextInt();
+		System.out.print("영어점수 입력: ");
+		this.engScore = s.nextInt();
+	}
+	public void print() {
+		String gpa;
+		double result = (korScore + mathScore + engScore)/3.0;
+		System.out.println("[" + this.name + "]님은");
+		System.out.printf("평균은 %.1f이고", result);
+		if(result < 60)
+			gpa = "F";
+		else if(result < 65)
+			gpa = "D";
+		else if(result < 70)
+			gpa = "D+";
+		else if(result < 75)
+			gpa = "C";
+		else if(result < 80)
+			gpa = "C+";
+		else if(result < 85)
+			gpa = "B";
+		else if(result < 90)
+			gpa = "B+";
+		else if(result < 95)
+			gpa = "A";
+		else
+			gpa = "A+";
+		System.out.println("학점은" + gpa + "입니다.");
+	}
+	public static void main(String args[]) {
+		new A06().run();
+	}
+}
+
+
+
+
